e8b317fcfd9a1d97f2de8b50fdf33c3a22c4d9f3
kihiuFrank <franklinekihiu@gmail.com>
2022-11-24 12:47:45 +0300

    components and styling

---
+++ README.md
@@ -0,0 +1,7 @@
+Front-end for Nft Marketplace 
+1. Home Page:
+    1. Show recently listed NFTs
+        1. If you own the NFT, you can update the listing
+        2. If not, you can buy the listing.
+2. Sell Page:
+    1. you can list your NFT on the marketplace
---
+++ components/Header.js
@@ -0,0 +1,20 @@
+import { ConnectButton } from "web3uikit"
+import Link from "next/link"
+
+export default function Header() {
+    return (
+        <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
+            <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1>
+            <div className="flex flex-row items-center">
+                <Link className="mr-4 p-6" href="/">
+                    Home
+                </Link>
+                <Link className="mr-4 p-6" href="/sell-nft">
+                    Sell NFT
+                </Link>
+
+                <ConnectButton moralisAuth={false} />
+            </div>
+        </nav>
+    )
+}
