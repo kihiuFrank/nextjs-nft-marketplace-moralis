c9cd604ece1b1ab33d7abcc133c3dd2ece5e6230
kihiuFrank <franklinekihiu@gmail.com>
2022-11-25 03:37:36 +0300

    styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
+import Head from "next/head"
 import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
 import Header from "../components/Header"
 
 function MyApp({ Component, pageProps }) {
     return (
-        <MoralisProvider initializeOnMount={false}>
-            <Header />
-            <Component {...pageProps} />
-        </MoralisProvider>
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <MoralisProvider initializeOnMount={false}>
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
-import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>NFT Marketplace</title>
-                <meta name="description" content="NFT Marketplace" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            Hi!
-        </div>
-    )
+    // How do we show the recently listed NFTs
+
+    // We will index the events off-chain and then read from our database.
+    // Setup a server to listen for those events to be fired, and we will add them to a database to query.
+
+    // TheGraph does this in a decentralised way
+    // Moralis does this in a centralised way and comes with a ton of other features.
+
+    // All our logic is 100% on chain
+    // Speed & Development time.
+    // Its really hard to start a prod blockchain project 100% decentralised.
+    // They are working on open sourcing their code.
+    // Feature Richness.
+    // We can create more features with a centralised back-end to start
+    // As more decentralised tools are being created.
+    // Local development.
+    return <div className={styles.container}>Hi!</div>
 }
